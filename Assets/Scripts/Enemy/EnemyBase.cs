1d0162bbe5b45619e47186036ee51a7caa2980bd
Chrystian C. Almeida <78819288+Chrystian-br@users.noreply.github.com>
2024-11-04 06:46:06 -0300

    Adding enemy
    Adicionando um inimigo básico com a capacidade de interagir com a vida do player, danificando-o ao contato.

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    #region VARIAVEIS
+        public int damage = 2;
+    #endregion
+     
+     
+    #region METODOS
+    
+    #endregion
+     
+     
+    #region UNITY-METODOS
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var health = collision.gameObject.GetComponent<HealthBase>();
+
+            Debug.Log(collision.transform.name);
+
+            if(health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+    #endregion
+}
